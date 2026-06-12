9068bd3cd48e89f54ce2e12b04ee3421d6373d3a
MOHD IMRAN <129662821+imranAoi@users.noreply.github.com>
2026-06-12 20:25:54 +0530

    Update link in Latest url

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -32,7 +32,7 @@ import until4 from '../assets/images/until4.png'
         </div>
         <div className="h-[1px] left-1/2 right-1/4 bg-[#1788ae] absolute top-1/2 hidden sm:block"></div>
         <div className="w-4 h-4 rounded-full border-[3px] border-[#ffe578] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"></div>
-        <a href="https://frontendof-ctl.vercel.app/" className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start">
+        <a href="https://ai-powered-ticket.vercel.app/" className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start">
           <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200 sm:ml-auto">
             <img className="max-w-[400px] w-full relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)]" src={until4} alt="" />
             <span className="flex group-hover:-top-14 ease-jump duration-200 text-black bg-[#ffe578] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
